94972149f8fcb60da0862416ba03bb5bc7d5d5c6
RafaelaGusczak <rafaela.gusczak@gmail.com>
2024-10-06 21:18:44 -0300

    Término da tela de Gerenciamento de usuarios

---
+++ src/html/gerenciamento.js
@@ -0,0 +1,8 @@
+function abrirMenu() {
+    var menu = document.getElementById('menuEditar');
+    if (menu.style.display === "none" || menu.style.display === "") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
